a3d7fc336231ec4553532a45227c18f87ae7865b
Azukho <mantsintsinis@gmail.com>
2023-09-19 01:37:47 +0300

    added "ALL" path, select drink, navbar/footer

--- index.js
+++ index.js
@@ -12,13 +12,51 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const APIKey = "1";
+
 app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({ extended : true }));
 
 app.get("/", async (req, res) => {
     try {
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-        const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+        var cocktails = [];
+
+        for(var i=0; i<alphabet.length; i++) {
+            
+            const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/${APIKey}/search.php?f=${alphabet[i]}`);
+            cocktails[i] = response.data.drinks;
+        }
+
+        res.render("all.ejs", { cocktails: cocktails });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.post("/drink", async (req, res) => {
+    try{
+        const cocktailID = req.body.cocktailID;
+    
+        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/${APIKey}/lookup.php?i=${cocktailID}`);
+        const cocktail = response.data.drinks[0];
+
+        const ingredients = [cocktail.strIngredient1, cocktail.strIngredient2, cocktail.strIngredient3, cocktail.strIngredient4, cocktail.strIngredient5, cocktail.strIngredient6, cocktail.strIngredient7, cocktail.strIngredient8, cocktail.strIngredient9, cocktail.strIngredient10, cocktail.strIngredient11, cocktail.strIngredient12, cocktail.strIngredient13, cocktail.strIngredient14, cocktail.strIngredient15];
+        const measures = [cocktail.strMeasure1, cocktail.strMeasure2, cocktail.strMeasure3, cocktail.strMeasure4, cocktail.strMeasure5, cocktail.strMeasure6, cocktail.strMeasure7, cocktail.strMeasure8, cocktail.strMeasure9, cocktail.strMeasure10, cocktail.strMeasure11, cocktail.strMeasure12, cocktail.strMeasure13, cocktail.strMeasure14, cocktail.strMeasure15];
+    
+    
+        res.render("index.ejs", {cocktail: cocktail, ingredients: ingredients, measures: measures});
+    } 
+    catch (error){
+        console.log(JSON.stringify(error));
+    }
+});
+
+app.get("/random", async (req, res) => {
+    try {
+
+        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/${APIKey}/random.php`);
 
         const cocktail = response.data.drinks[0];        
 
